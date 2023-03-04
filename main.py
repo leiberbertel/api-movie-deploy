0cda0fceb01a0494c0159be96ca7d0ad38821046
leiber bertel <leiberbertel777@gmail.com>
2023-03-04 10:55:51 -0500

    add file, main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from config.database import engine, Base
+from middlewares.error_handler import ErrorHandler
+from routers.movie import movie_router
+from routers.users import user_router
+import os
+import uvicorn
+
+app = FastAPI()
+app.title = "Mi aplicación con  FastAPI"
+app.version = "0.0.1"
+
+app.add_middleware(ErrorHandler)
+
+app.include_router(movie_router)
+app.include_router(user_router)
+
+Base.metadata.create_all(bind=engine)
+
+
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
+
+@app.get('/', tags=['home'])
+def message():
+    return HTMLResponse('<h1>Hello world</h1>')
+
+
+if __name__ == "__main__":
+	uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
